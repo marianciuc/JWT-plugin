2ce1f75a034cbbd6e55c6d53d87d0c18c69c75a9
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-29 00:00:18 +0200

    Remove UserDetailsException and related tests
    Deleted the UserDetailsException class and its associated test cases in UserServiceTest. This cleanup aims to eliminate unused code and improve maintainability.

--- src/main/java/io/github/marianciuc/jwtsecurity/exceptions/UserDetailsException.java
+++
@@ -1,7 +0,0 @@
-package io.github.marianciuc.jwtsecurity.exceptions;
-
-public class UserDetailsException extends RuntimeException {
-    public UserDetailsException(String message) {
-        super(message);
-    }
-}
